81d2ad2b9e0bc51863186bda0160b8180b2d6a16
Adam Edwards <adamedx@gmail.com>
2020-02-07 17:55:36 -0800

    Simplify DisplayTypeFormatter usage to avoid need for throwaway class instance

--- src/REST/RequestLogEntry.ps1
+++ src/REST/RequestLogEntry.ps1
@@ -25,7 +25,8 @@ ScriptClass RequestLogEntry {
         const ERROR_MESSAGE_EXTENDED_FIELD 'ErrorMessage'
         const EXTENDED_PROPERTIES @($ERROR_MESSAGE_EXTENDED_FIELD)
 
-        $logFormatter = new-so DisplayTypeFormatter $LOG_ENTRY_DISPLAY_TYPE 'RequestTimestamp', 'StatusCode', 'Method', 'Uri'
+        $::.DisplayTypeFormatter |=> RegisterDisplayType $LOG_ENTRY_DISPLAY_TYPE 'RequestTimestamp', 'StatusCode', 'Method', 'Uri'
+
         $ExtendedPropertySet = $null
 
         function GetExtendedPropertySet {
